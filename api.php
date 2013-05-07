12183855a35e959dc1486b0d3ba94500ffc3b0ba
djmattyg007 <mwgam36@gmail.com>
2013-05-08 08:33:32 +1000

    Started routing code and re-worked API auth.

--- api.php
+++ api.php
@@ -5,6 +5,11 @@ define("MODE", "API");
 // Initialise response functions.
 require("response.php");
 
+// The client did not specify which method they were calling.
+if (!isset($_GET["q"])) {
+	respond(400, false, "No request method was specified.");
+}
+
 // If no API key was supplied, no further processing should be done.
 if (!isset($_SERVER["HTTP_X_API_KEY"])) {
 	respond(400, false, "There was no API key supplied with the request.");
@@ -13,9 +18,36 @@ if (!isset($_SERVER["HTTP_X_API_KEY"])) {
 // Setup site configuration.
 require("config.php");
 
+// Start routing.
+$request = explode("/", $_GET["q"]); // Break down request URI.
+$requestType = $request[0]; // Explicitly grab the first segment of the request URI (the endpoint name).
+$endpoint = "endpoints" . DIRECTORY_SEPARATOR . $requestType . ".php"; // Construct the endpoint filename.
+// Make sure the endpoint exists.
+if (!file_exists($endpoint)) {
+	respond(404, false, "The requested method could not be found.");
+}
+$methodRegistry = array(); // Used to set whether or not a method requires authentication.
+require($endpoint); // Initialise the method registry and the endpoint's methods.
+
+$requestMethod = $request[1]; // Explicitly grab the second segment of the request URI (the endpoint method).
+$requestFunction = $requestType . "_" . $requestMethod; // Construct the endpoint method's function name.
+// Make sure the endpoint method exists.
+if (!function_exists($requestFunction)) {
+	respond(404, false, "The requested method could not be found.");
+}
+// Make sure the endpoint method's registry entry exists.
+if (!isset($methodRegistry[$requestFunction])) {
+	respondFatal();
+}
+
 // Setup database connection.
 require("db" . DIRECTORY_SEPARATOR . "connection.php");
 
-// Perform API authorisation check.
-require("db" . DIRECTORY_SEPARATOR . "auth.php");
+// If the endpoint method requires authentication, check to see whether or not we have it.
+if ($methodRegistry[$requestFunction]) {
+	verifySession();
+}
+
+// We are now ready to actually process the client's request.
+call_user_func($requestFunction);
 
