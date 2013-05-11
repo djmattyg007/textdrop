5c1b53958f363cf9e24f4cfa77a0a32534655575
djmattyg007 <mwgam36@gmail.com>
2013-05-11 13:05:26 +1000

    Added code for session token verification.

--- db/connection.php
+++ db/connection.php
@@ -15,7 +15,39 @@ try {
 
 function verifySession()
 {
-	//TODO: actually write the code
-	return null;
+	try {
+		$statement = $db->prepare("SELECT created, key, expiry FROM sessions WHERE token = ?");
+		$statement->bindParam(1, $_POST["token"], PDO::PARAM_STR);
+		$statement->execute();
+		$session = $statement->fetch(PDO::FETCH_BOTH);
+		unset($statement);
+	} catch (PDOException $e) {
+		//TODO: examine code & message in exception
+		respond(500, false, "Unidentified database error.");
+	}
+
+	if (empty($session)) {
+		//TODO: check error code
+		respond(400, false, "Session does not exist.");
+	}
+	if ($session["key"] !== $_SERVER["HTTP_X_API_KEY"]) {
+		respond(400, false, "Session token and API key do not match.");
+	}
+	if (strtotime($_POST["createdAt"]) > strtotime($session["expiry"])) {
+		cleanSessions();
+		//TODO: check error code
+		respond(401, false, "Session has expired.");
+	}
+
+	try {
+		$statement = $db->prepare("UPDATE sessions SET expiry = ? WHERE token = ?");
+		$statement->bindParam(1, $_POST["createdAt"], PDO::PARAM_STR);
+		$statement->bindParam(2, $_POST["token"], PDO::PARAM_STR);
+		$statement->execute();
+	} catch (PDOException $e) {
+		respond(500, false, "Unidentified database error.");
+	}
+	return true;
 	respond(401, false, "Unable to verify session token.");
 }
+
