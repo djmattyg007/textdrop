e15a77e1f71fff55ef037c9d409edd49c009b6a3
djmattyg007 <mwgam36@gmail.com>
2013-05-15 18:29:47 +1000

    Added more log file config values, and documented them all.

--- config.sample.php
+++ config.sample.php
@@ -6,8 +6,18 @@ if (!defined("MODE")) {
 $CONFIG = array();
 
 $CONFIG["APP_NAME"] = "TextDrop";
+
+// Enter the directory to store the logs.
+// No trailing slashes!
 $CONFIG["LOG_ACCESS"] = "";
 $CONFIG["LOG_ERROR"] = "";
+// Maximum size for a log file (in Megabytes) before it is rotated.
+$CONFIG["LOG_SIZE"] = 5;
+// Maximum number of logs to keep before the eldest is deleted.
+// This number includes the current log.
+$CONFIG["LOG_MAX"] = 10;
+// If you want to disable logging, do not set LOG_MAX to zero.
+// Instead, simply leave the config entries for the logs you do not want active as empty strings.
 
 $CONFIG["DB_HOST"] = "";
 $CONFIG["DB_NAME"] = "";
