43c44dde8f380d06f8d16fd11283efe526754acd
djmattyg007 <mwgam36@gmail.com>
2013-06-27 08:42:24 +1000

    Actually added todo to working comments.
    This probably shouldn't be a separate commit but oh well

--- translation/translate.php
+++ translation/translate.php
@@ -1,19 +1,19 @@
 <?php
 
 if (!empty($CONFIG["LANG"])) {
-	//respond with error
+	//TODO: respond with error
 }
 
 $translationFile = $CONFIG["LANG"] . ".json";
 if (!file_exists($translationFile) {
-	//respond with error
+	//TODO: respond with error
 }
 
 $translation = file_get_contents($translationFile);
 $translation = json_decode($translation);
 
 if (!$translation) {
-	//respond with error
+	//TODO: respond with error
 }
 
 function translate($str)
