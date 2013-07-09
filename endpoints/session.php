1565ec1cb4297933031c8f9367bc2d1a79a249d9
djmattyg007 <mwgam36@gmail.com>
2013-07-09 20:10:29 +1000

    Fixed derp.

--- endpoints/session.php
+++ endpoints/session.php
@@ -137,7 +137,7 @@ function session_login()
 	$response["session"]["token"] = $sessionToken;
 	$response["session"]["active"] = array();
 	$response["session"]["active"]["key"] = ($sessionKeyTotal + 1);
-	$response["session"]["active"]["user"] = (count($findKey["all"] + 1);
+	$response["session"]["active"]["user"] = (count($findKey["all"]) + 1);
 	respond(200, true, translate("New session created successfully."), $response);
 }
 
