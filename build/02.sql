2177d14accb7836042fbc60f2ca529a126ce0f29
djmattyg007 <mwgam36@gmail.com>
2013-08-01 18:22:04 +1000

    Changed names of constraints in 2nd build script.

--- build/02.sql
+++ build/02.sql
@@ -10,14 +10,14 @@ CREATE TABLE friendships (
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 ALTER TABLE `users`
-ADD CONSTRAINT userActive CHECK `active` IN (0, 1);
+ADD CONSTRAINT userActiveCheck CHECK `active` IN (0, 1);
 
 ALTER TABLE `api_keys`
-ADD CONSTRAINT keyActive CHECK `active` IN (0, 1);
+ADD CONSTRAINT keyActiveCheck CHECK `active` IN (0, 1);
 
 ALTER TABLE `sessions`
 ADD CONSTRAINT requestsCheck CHECK `totalRequests` >= 0;
 
 ALTER TABLE `main_data`
-ADD CONSTRAINT archived CHECK `archived` IN (0, 1);
+ADD CONSTRAINT archivedCheck CHECK `archived` IN (0, 1);
 
