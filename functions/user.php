a86145c33aa3ecc0e696b1d17218a5d161be3e8d
djmattyg007 <mwgam36@gmail.com>
2013-08-01 18:41:22 +1000

    Added code to draw entire user into memory and cache it.

--- functions/user.php
+++ functions/user.php
@@ -2,13 +2,16 @@
 if (!defined("MODE")) {
 	exit("No direct script access allowed.");
 }
-//TODO: draw an entire user into memory and cache it
 
-function userf_exists($userID, $fullUser = false)
+$userf = null;
+
+// Draw an entire data into memory and cache it. This reduces the number of
+// requests to the database.
+function userf_draw($userID)
 {
 	if (is_numeric($_POST["user"])) {
 		if (intval($_POST["user"]) <= 0) {
-			return null;
+			return false;
 		}
 		$where = "userID";
 		$whereType = PDO::PARAM_INT;
@@ -18,14 +21,8 @@ function userf_exists($userID, $fullUser = false)
 	}
 	global $db;
 
-	if ($fullUser) {
-		$select = "`userID`, `username`, `displayname`";
-	} else {
-		$select = "`userID`";
-	}
-
 	try {
-		$statement = $db->prepare("SELECT $select FROM `users` WHERE `{$where}` = ?");
+		$statement = $db->prepare("SELECT `userID`, `username`, `displayname` FROM `users` WHERE `{$where}` = ?");
 		$statement->bindParam(1, $_POST["user"], $whereType);
 		$statement->execute();
 		$user = $statement->fetch(PDO::FETCH_ASSOC);
@@ -34,9 +31,27 @@ function userf_exists($userID, $fullUser = false)
 		logEntry("ERROR", "now", 500, __FUNCTION__, $e);
 		respond(500, false, translate("Unidentified database error."));
 	}
-
+	
 	if ($user) {
-		return $user;
+		if ($GLOBALS["userf"] === null) {
+			$GLOBALS["userf"] = array();
+		}
+		$GLOBALS["userf"][$user["userID"]] = &$user;
+		$GLOBALS["userf"][$user["username"]] = &$user;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function userf_exists($userID, $fullUser = false)
+{
+	if (userf_draw($userID)) {
+		if ($fullUser) {
+			return $GLOBALS["userf"][$userID];
+		} else {
+			return $GLOBALS["userf"][$userID]["userID"];
+		}
 	} else {
 		return null;
 	}
