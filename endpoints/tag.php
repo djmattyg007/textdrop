04499caf155b444426655f679b54d99ab7ed8212
djmattyg007 <mwgam36@gmail.com>
2013-08-07 18:24:57 +1000

    Added tag endpoint with tag_get endpoint method.

---
+++ endpoints/tag.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined("MODE")) {
+	exit("No direct script access allowed.");
+}
+
+$methodRegistry["tag_get"] = true;
+//TODO: add friendships
+function tag_get()
+{
+	if (empty($_POST["tagID"])) {
+		respond(400, false, translate("There was no tag ID supplied with the request."));
+	}
+	if (!is_numeric($_POST["tagID"])) {
+		respond(400, false, translate("Invalid tag ID supplied with the request."));
+	}
+	$tagID = intval($_POST["tagID"]);
+	if (!tagf_draw($tagID)) {
+		respond(400, false, translate("There was no tag matching the ID supplied with the request."));
+	}
+
+	$response = array();
+	$response["request"] = array();
+	$response["request"]["tag"] = $GLOBALS["tagf"][$tagID];
+	$response["session"] = array();
+	$response["session"]["expiryTime"] = $GLOBAL["EXPIRYTIME"];
+	respond(200, true, translate("Requested data successfully retrieved."), $response);
+}
+
